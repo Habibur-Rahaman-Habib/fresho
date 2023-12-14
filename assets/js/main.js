d916078e06483a4e271c8202a19d237d5241ca9a
Habibur Rahaman <habib5501659@gmail.com>
2023-12-14 07:41:33 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -392,13 +392,15 @@ $(document).ready(function () {
 	var swiper = new Swiper(".mySwiper", {
 		loop: true,
 		spaceBetween: 10,
-		slidesPerView: 4,
-		freeMode: true,
+		slidesPerView: 3,
+		// freeMode: true,
 		watchSlidesProgress: true,
+		centeredSlides: true,
 	});
 
 	var swiper = new Swiper(".mySwiper2", {
 		loop: true,
+		// slidesPerView: 1,
 		spaceBetween: 10,
 		navigation: {
 			nextEl: ".swiper-button-next",
